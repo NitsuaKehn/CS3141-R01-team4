65af2c98ca3bf41a8d451fa434154a0e56ee7c1a
kikwi333 <63129127+kikwi333@users.noreply.github.com>
2022-02-23 17:46:36 -0500

    Improper JavaFX lib fixed

--- src/com/team_software/Main.java
+++ src/com/team_software/Main.java
@@ -1,12 +1,41 @@
 package com.team_software;
 
-public class Main {
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
     public static void main(String[] args) {
+        //Launches window
+        launch(args);
+    }
 
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        //Creates the main window titled "Hello World!"
+        primaryStage.setTitle("Hello World!");
 
+        //Creates a button titled "Say 'Hello World!"
+        //Button will then print "Hello World!" in stdout
+        Button btn = new Button();
+        btn.setText("Say 'Hello World!'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        //Add button to window positioned at v:300 and v1:250
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
     }
 }
