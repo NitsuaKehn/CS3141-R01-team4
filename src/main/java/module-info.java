0243f42962d8afafe921d3757bf3ced34f0dc12b
kikwi333 <63129127+kikwi333@users.noreply.github.com>
2022-02-23 21:09:39 -0500

    Made JavaFX functional

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.team_software.cs3141_project {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.team_software.cs3141_project to javafx.fxml;
+    exports com.team_software.cs3141_project;
+}
