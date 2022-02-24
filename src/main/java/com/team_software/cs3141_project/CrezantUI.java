ddcd66acf4b752d91b723e69c472e46dc1448eab
kikwi333 <63129127+kikwi333@users.noreply.github.com>
2022-02-23 22:15:26 -0500

    Changed throwing IOException to Exception Replaced actionEvent statement with a shorthanded lambda statement

--- src/main/java/com/team_software/cs3141_project/CrezantUI.java
+++ src/main/java/com/team_software/cs3141_project/CrezantUI.java
@@ -18,16 +18,11 @@ public class CrezantUI extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception {
         stage.setTitle("Crezant");
         Button btn = new Button();
         btn.setText("Say 'Hello World!");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                System.out.println("Hello World!");
-            }
-        });
+        btn.setOnAction(actionEvent -> System.out.println("Hello World!"));
 
         StackPane root = new StackPane();
         root.getChildren().add(btn);
