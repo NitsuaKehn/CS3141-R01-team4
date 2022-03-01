47e8b8cad1f69c469f45c265eaff04306962961f
relind <roger.e.lind@gmail.com>
2022-03-01 12:07:47 -0500

    implemented displaytext method and updated the main method as needed

--- src/main/java/com/team_software/cs3141_project/CrezantUI.java
+++ src/main/java/com/team_software/cs3141_project/CrezantUI.java
@@ -4,9 +4,15 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 import java.io.File;
@@ -26,20 +32,87 @@ public class CrezantUI extends Application {
         Button btn = new Button();
         btn.setText("Say 'Hello World!");
         btn.setOnAction(actionEvent -> System.out.println("Hello World!"));
+        //root.getChildren().add(btn);
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        stage.setScene(new Scene(root, 300, 250));
+
+        VBox messagesFeild = new VBox();//vbox to hold sent and received messages
+        messagesFeild.setSpacing(10);
+        messagesFeild.prefWidthProperty().bind(stage.widthProperty());//makes the vbox always as big as the stage
+        messagesFeild.setBackground(new Background(new BackgroundFill(Paint.valueOf("#26007A"), null, null)));//sets the background color
+
+
+
+        ScrollPane scroller = new ScrollPane();//pane that allows scrolling
+        scroller.fitToHeightProperty().set(true);//makes the scrollpane be the total vertical size
+        scroller.vbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.ALWAYS);//makes the side scrollbar show up
+        scroller.hbarPolicyProperty().setValue(ScrollPane.ScrollBarPolicy.NEVER);//makes the bottom scroll bar hidden
+
+        //file that holds the messages info
+        File infile = new File("test_text_info.txt");
+
+        //sets up the pane with all the messages in it
+        displayText(infile, messagesFeild);
+
+        //adds the vbox with the messages to the scroller
+        scroller.setContent(messagesFeild);
+
+        stage.setScene(new Scene(scroller, 300, 250));
         stage.show();
     }
 
     //gets the messages from the text file and adds them to the feild
-    public void displayText(File file)
+    public void displayText(File file, Pane root)
     {
+        //opens new scanner
         try(Scanner in = new Scanner(file))
         {
+            //loops until file is empty
+            while(in.hasNext()) {
+
+                //stores wheather the message should be left aligned
+                Boolean leftAlign;
+
+                //checks whather there is an R(recived) at the begining of the line
+                if (in.next().equals("R")) {
+                    leftAlign = true;
+                }
+                //means there is a S(sent)
+                else
+                {
+                    leftAlign = false;
+                }
+
+                //creates temp label
+                Label temp = new Label(in.nextLine());
+                //adds padding so the scroll bar doesn't cover words
+                temp.setPadding(new Insets(5,50,5,15));
+                //adds the background
+                temp.setBackground(new Background(new BackgroundFill(Paint.valueOf("#4B0082"), new CornerRadii(20), new Insets(0,45,0,10))));
+                //sets the text color
+                temp.setTextFill(Paint.valueOf("#006FFF"));
+
+                //creates an hbox to store the label so we can set the alignment
+                HBox hbox = new HBox();
+                //adds the label to the hbox
+                hbox.getChildren().add(temp);
+
+                //checks the alignment type and set the apropiate one
+                if(!leftAlign)
+                {
+                    hbox.setAlignment(Pos.BASELINE_RIGHT);
+                }
+                else
+                {
+                    hbox.setAlignment(Pos.BASELINE_LEFT);
+                }
+
+                //adds the hbox to the given pane
+                root.getChildren().add(hbox);
+
+            }
 
         }
+        //file not found exception
         catch(FileNotFoundException e)
         {
             System.out.println("file not found");
