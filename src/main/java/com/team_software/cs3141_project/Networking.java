c7f8cee1d8600047cb33e2203d5221b27dbf3f48
kikwi333 <63129127+kikwi333@users.noreply.github.com>
2022-03-17 16:22:40 -0400

    WIP Networking

--- src/main/java/com/team_software/cs3141_project/Networking.java
+++ src/main/java/com/team_software/cs3141_project/Networking.java
@@ -9,4 +9,16 @@ import javax.swing.JOptionPane;
 
 public class Networking {
 
+    public static void main(String[] args) throws IOException {
+        String IP = JOptionPane.showInputDialog("Input Your IP Server: ");
+        ServerSocket serverSock = new ServerSocket(6066);
+        Socket Sock = serverSock.accept();
+        DataOutputStream out = new DataOutputStream(Sock.getOutputStream());
+        out.writeUTF("i am fine, thank you");
+        DataInputStream in = new DataInputStream(Sock.getInputStream());
+        System.out.println(in.readUTF());
+        Sock.close();
+    }
+
+
 }
